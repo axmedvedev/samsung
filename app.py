18e2fadaa7cca9fb15c39e3385f40526e31bf738
Alexander <sashka.gm@gmail.com>
2023-10-27 17:04:51 +0600

    add telegram bot

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+import requests
 
 app = Flask(__name__)
     
@@ -8,7 +10,21 @@ with app.app_context():
     @app.route('/')
     def index():
         return render_template('index.html')
-
+    
+    @app.route('/care-service', methods=['POST'])
+    def care_service():
+        if request.method == 'POST':
+            request_data = request.get_json()
+            message = f"Имя: {request_data['name']}\nТелефон: {request_data['phone']}\nСообщение: {request_data['message']}"
+            token_bot = '6551926073:AAEi8rDWlAMYv_tQLjSQXC6nrvkbBvn4YIo'
+            query_string = (
+                f'&chat_id=-963225760'
+                f'&text={message}'
+                f'&parse_mode=HTML'
+                f'&disable_web_page_preview=True'
+            )
+            requests.get(f"https://api.telegram.org/bot{token_bot}/sendMessage?{query_string}")
+        return b'{"status": "ok"}'
 
 
 
