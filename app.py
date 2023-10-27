d035ac892de3effa59b2b55f52648bf79506f129
Alexander <sashka.gm@gmail.com>
2023-10-27 17:10:25 +0600

    add telegram bot

--- app.py
+++ app.py
@@ -13,18 +13,21 @@ with app.app_context():
     
     @app.route('/care-service', methods=['POST'])
     def care_service():
-        if request.method == 'POST':
-            request_data = request.get_json()
-            message = f"Имя: {request_data['name']}\nТелефон: {request_data['phone']}\nСообщение: {request_data['message']}"
-            token_bot = '6551926073:AAEi8rDWlAMYv_tQLjSQXC6nrvkbBvn4YIo'
-            query_string = (
-                f'&chat_id=-963225760'
-                f'&text={message}'
-                f'&parse_mode=HTML'
-                f'&disable_web_page_preview=True'
-            )
-            requests.get(f"https://api.telegram.org/bot{token_bot}/sendMessage?{query_string}")
-        return b'{"status": "ok"}'
+        try:
+            if request.method == 'POST':
+                request_data = request.get_json()
+                message = f"Имя: {request_data['name']}\nТелефон: {request_data['phone']}\nСообщение: {request_data['message']}"
+                token_bot = '6551926073:AAEi8rDWlAMYv_tQLjSQXC6nrvkbBvn4YIo'
+                query_string = (
+                    f'&chat_id=-963225760'
+                    f'&text={message}'
+                    f'&parse_mode=HTML'
+                    f'&disable_web_page_preview=True'
+                )
+                requests.get(f"https://api.telegram.org/bot{token_bot}/sendMessage?{query_string}")
+            return b'{"status": "ok"}'
+        except Exception as e:
+            return b'{"status": "error"}'
 
 
 
